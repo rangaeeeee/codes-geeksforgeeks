01bd9304b1d076c96c4f336bc82cb990143d78ad
Proud Indian <rrangarajan.85@gmail.com>
2017-09-05 15:12:57 +0530

    Update binary-tree.py

--- data-structures/binary-tree.py
+++ data-structures/binary-tree.py
@@ -7,6 +7,22 @@ class node():
 class binaryTree():
   def __init__(self):
     self.head = None
+ 
+  def insertNode(self, data):
+
+    if self.data:
+      if data < self.data:
+        if self.left is None:
+          self.left = node(data)
+        else:
+          self.left.insertNode(data)
+      elif data > self.data:
+          if self.right is None:
+            self.right = node(data)
+          else:
+            self.right.insertNode(data)
+    else:
+      self.data = data    
 
   def printPreorder(self,node):
     if node == None:
@@ -30,16 +46,30 @@ class binaryTree():
     print(node.data)    
     
   def printMenu(self):
-      menuDict = [
-          ('add node',addNode),
-          'print pre-order tree':printPreorder,
-          'print in-order tree':printInorder,
-          'print post-order tree':printPostorder
-          }
-      i = 1
-      for x in menuList:
-        print(str(i) + " " +x)
-        i += 1
+      menuList = [
+          ['add node','insertNode'],
+          ['print pre-order tree','printPreorder'],
+          ['print in-order tree','printInorder'],
+          ['print post-order tree 2','printPostorder']
+          ]
+
+      for x in enumerate(menuList):
+         print (str(x[0]) + ". "  + str(x[1]))
+      print("before choice")
+      choice = 0
+      #choice = int(input())
+      print("choice : " + str(choice))
+      if menuList[choice][1] == 'insertNode':
+        print("enter the node data : ")
+        data = int(input())
+        insertNode(data)
+      elif menuList[choice][1] == 'printPreorder':
+        printPreorder()
+      elif menuList[choice][1] == 'printPostorder':
+        printPostorder()
+      elif menuList[choice][1] == 'printInorder':
+        printInorder()
+
 
 def main():
   bt = binaryTree()
